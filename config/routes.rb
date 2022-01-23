4b23fa0e9b8d1a5d642db9a9ef94772e6991e8f7
willand <willanddadia@gmail.com>
2022-01-23 19:34:38 +0800

    add oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  use_doorkeeper
+  devise_for :users
+  resources :bookmarks
+  root 'bookmarks#index'
+
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
+
+  namespace :api do
+    resources :users, only: %i[create]
+    resources :bookmarks, only: %i[index]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
